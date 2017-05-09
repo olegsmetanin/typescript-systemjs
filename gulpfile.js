a5a6167d9bdb0ce336aec7e4675ff47967da2b0c
Oleg Smetanin <oleg@smetan.in>
2017-05-09 22:25:14 +0300

    improve compilation

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ var del = require('del');
 var spawn = require('child_process').spawn;
 var gutil = require('gulp-util');
 var browserSync = require('browser-sync').create();
+var debounce = require('lodash.debounce');
+var browserReload =  debounce(browserSync.reload, 1000);
+
+var tscCompilationCompleteRegex = / Compilation complete\. Watching for file changes\./;
+var typescriptErrorRegex = /\(\d+,\d+\): error TS\d+:/;
 
 gulp.task('clean', function() {
     return del(['public', 'build']);
@@ -16,24 +21,18 @@ gulp.task('assets', function() {
 });
 
 gulp.task('watch', function() {
-    watch('./src/assets/**/*')
-        .on('change', (event) => {
-            runSequence('assets', () => {
-                browserSync.reload();
-            });
-        });
-
-    watch('./public/js/**/*')
-        .on('change', (event) => {
-            browserSync.reload();
-        });
+    // Original gulp.watch too slow
+    // watch('./public/**/*')
+    //     .on('change', (event) => {
+    //         browserReload();
+    //     });
 });
 
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
+            open: false,
             baseDir: "./public",
-            reloadDebounce: 1000,
             routes: {
                 "/node_modules": "node_modules"
             }
@@ -42,16 +41,27 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('tsc', function (cb) {
+    var firstCompilation = true;
+    // typescript watch is the best one
     var tsc = spawn(require.resolve('./node_modules/.bin/tsc'), ['--module', 'amd', '--target', 'ES5', '--jsx', 'react', '--sourceMap', '--outDir', './public/js', '-w', './src/index.tsx']);
 
     tsc.stdout.on('data', (data) => {
         var dataStr = data.toString();
-        if (dataStr.toLowerCase().indexOf('error') === -1) {
-            gutil.log('[tsc] ' + dataStr);
-        } else {
+        if (typescriptErrorRegex.test(dataStr)) {
+            // tsc otput errors to stdout!
             gutil.log(gutil.colors.red('[tsc:error] '+ dataStr));
+        } else {
+            gutil.log('[tsc] ' + dataStr);
         }
-        browserSync.reload();
+
+        if (firstCompilation) {
+            firstCompilation = false;
+            cb();
+        } else {
+            if (tscCompilationCompleteRegex.test(dataStr)) {
+                browserReload();
+            }
+        }
     });
 
     tsc.stderr.on('data', (data) => {
@@ -61,16 +71,14 @@ gulp.task('tsc', function (cb) {
     tsc.on('close', (code) => {
         gutil.log(`[tsc] exited with code ${code}`);
     });
-
-    cb();
 })
 
 gulp.task('serve', function(callback) {
     runSequence(
         'assets',
-        'watch',
         'tsc',
         'browser-sync',
+        'watch',
         callback
     );
 });
--- src/ExampleComponent.tsx
+++ src/ExampleComponent.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 export class ExampleComponent extends React.Component<any, void> {
   render() {
-    return <div>Hello World!</div>
+    return <div>!Hello World!</div>
   }
 }
 
